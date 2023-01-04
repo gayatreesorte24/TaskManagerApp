1f6616b5597c2f163faaecec73e5d347ff1328e9
gayatreesorte24 <gayatreesorte@gmail.com>
2023-01-04 19:06:05 +0530

    Intial Project Structure

---
+++ TaskManagerAppBE/src/main/java/com/task/taskmanagerappbe/TaskManagerAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.task.taskmanagerappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerAppBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerAppBeApplication.class, args);
+    }
+
+}
---
+++ TaskManagerAppBE/src/test/java/com/task/taskmanagerappbe/TaskManagerAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.taskmanagerappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
